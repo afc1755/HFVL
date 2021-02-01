84eed7db6b0d64ea49f8de305de4360325d7f06b
Andrew Chabot <afc1755@rit.edu>
2021-02-01 13:20:14 -0500

    modifyArrow is working

--- smallParserBoxAndArrow.py
+++ smallParserBoxAndArrow.py
@@ -7,22 +7,24 @@ WINDOW_WIDTH = 1200
 WINDOW_HEIGHT = 800
 SMALL_BOX_WIDTH = 120
 SMALL_BOX_HEIGHT = 100
+SMALL_BOX_BORDER_NORMAL = 2
+SMALL_BOX_BORDER_BOLD = 4
 START_BOX_POS = [10, 10]
 
 WHITE = (255, 255, 255)
 WHITE_ALPHA = (255, 255, 255, 255)
 BLACK = (0, 0, 0)
 BLACK_ALPHA = (0, 0, 0, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+RED = (180, 0, 0)
+GREEN = (0, 150, 0)
+BLUE = (0, 0, 150)
 
 DEFAULT_TEXT_SIZE = 10
 
 ARROW_NORMAL_SIZE = 10
 ARROW_BOLD_SIZE = 12
-LINE_NORMAL_SIZE = 1
-LINE_BOLD_SIZE = 2
+LINE_NORMAL_SIZE = 2
+LINE_BOLD_SIZE = 4
 
 
 def read_file(file_to_read):
@@ -72,7 +74,6 @@ def read_file(file_to_read):
                                 user_box_color = RED
                             else:
                                 print("invalid color used, defaulting to black\ncolor options are: blue, green, red")
-                #dummy_arr = draw_box(x, y, width, height, foreground_batch, text_batch, box_text, box_bold, user_box_color, dummy_arr)
                 box_dict[box_id] = [x, y, width, height, box_text, box_bold, user_box_color]
             elif function_line[0] == '$drawArrow':
                 arrow_args = function_line[1].split(',')
@@ -105,7 +106,7 @@ def read_file(file_to_read):
                 y2 = box_dict[end_box_id][1] + box_dict[end_box_id][3]
                 arrow_color.append(user_arr_color)
                 arrowline_dict[start_box_id+end_box_id] = [x1, y1, x2, y2, arr_bold, user_arr_color]
-                arrowheads.append(draw_arrow(x1, y1, x2, y2, arr_bold, user_arr_color, foreground_batch, dummy_arr))
+
             elif function_line[0] == '$modifyBox':
                 box_text = ''
                 box_args = function_line[1].split(',')
@@ -123,13 +124,37 @@ def read_file(file_to_read):
                             box_dict[box_id][5] = False
                     elif attribute_args[0].strip().lower() == 'color':
                         if attribute_args[1].strip('\'').lower() == 'blue':
-                            box_dict[box_id][6] = BLUE
+                            arrowline_dict[box_id][6] = BLUE
                         elif attribute_args[1].strip('\'').lower() == 'green':
                             box_dict[box_id][6] = GREEN
                         elif attribute_args[1].strip('\'').lower() == 'red':
                             box_dict[box_id][6] = RED
                         else:
                             print("invalid color used, defaulting to black\ncolor options are: blue, green, red")
+
+            elif function_line[0] == '$modifyArrow':
+                arrow_args = function_line[1].split(',')
+                start_box_id = arrow_args[0].replace('\'', '')
+                end_box_id = arrow_args[1].replace('\'', '')
+                if (start_box_id + end_box_id) not in arrowline_dict:
+                    print('boxes not found for arrow to go between\nbox ids: ' + start_box_id + ' and ' + end_box_id)
+                if len(arrow_args) > 2:
+                    for i in range(2, len(arrow_args)):
+                        attribute_args = arrow_args[i].split('=')
+                        if attribute_args[0].strip().lower() == 'bold':
+                            if attribute_args[1].strip('\'').lower() == 'true':
+                                arrowline_dict[start_box_id + end_box_id][4] = True
+                            else:
+                                arrowline_dict[start_box_id + end_box_id][4] = False
+                        elif attribute_args[0].strip().lower() == 'color':
+                            if attribute_args[1].strip('\'').lower() == 'blue':
+                                arrowline_dict[start_box_id + end_box_id][5] = BLUE
+                            elif attribute_args[1].strip('\'').lower() == 'green':
+                                arrowline_dict[start_box_id + end_box_id][5] = GREEN
+                            elif attribute_args[1].strip('\'').lower() == 'red':
+                                arrowline_dict[start_box_id + end_box_id][5] = RED
+                            else:
+                                print("invalid color used, defaulting to black\ncolor options are: blue, green, red")
             else:
                 print('error while parsing\ninvalid function at line: \n\t' + line)
 
@@ -138,7 +163,7 @@ def read_file(file_to_read):
         window.clear()
         background_batch.draw()
         for curr_box in box_dict:
-            if box_dict[curr_box][5] != '':
+            if box_dict[curr_box][4] != '':
                 dummy_arr.append(pyglet.text.Label(box_dict[curr_box][4],
                                                    font_size=DEFAULT_TEXT_SIZE,
                                                    x=box_dict[curr_box][0] + DEFAULT_TEXT_SIZE,
@@ -147,11 +172,12 @@ def read_file(file_to_read):
                                                    batch=text_batch))
             dummy_arr.append(shapes.BorderedRectangle(box_dict[curr_box][0], box_dict[curr_box][1],
                                                       box_dict[curr_box][2], box_dict[curr_box][3],
-                                                      border=1,
+                                                      border=SMALL_BOX_BORDER_BOLD if box_dict[curr_box][5]
+                                                      else SMALL_BOX_BORDER_NORMAL,
                                                       color=WHITE,
                                                       border_color=box_dict[curr_box][6],
                                                       batch=foreground_batch))
-        #arrowline_dict[start_box_id+end_box_id] = [x1, y1, x2, y2, arr_bold, user_arr_color]
+
         for arrowline in arrowline_dict:
             dummy_arr.append(shapes.Line(arrowline_dict[arrowline][0], arrowline_dict[arrowline][1],
                                          arrowline_dict[arrowline][2], arrowline_dict[arrowline][3],
@@ -177,28 +203,6 @@ def read_file(file_to_read):
             arrowheads[curr_arrow].draw(GL_TRIANGLES)"""
 
     pyglet.app.run()
-
-
-def draw_box(x, y, width, height, use_batch, text_batch, text, bold, color, dummy_arr):
-    if text != '':
-        dummy_arr.append(pyglet.text.Label(text,
-                                           font_size=DEFAULT_TEXT_SIZE,
-                                           x=x + DEFAULT_TEXT_SIZE,
-                                           y=y + height/2,
-                                           color=color + (255,),
-                                           batch=text_batch))
-
-    dummy_arr.append(shapes.BorderedRectangle(x, y,
-                                              width, height,
-                                              border=1,
-                                              color=WHITE,
-                                              border_color=color,
-                                              batch=use_batch))
-    return dummy_arr
-
-
-def draw_arrow(x1, y1, x2, y2, bold, color, use_batch, dummy_arr):
-    print('test')
 
 
 if __name__ == "__main__":
